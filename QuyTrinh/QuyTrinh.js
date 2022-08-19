20978f839b3b7642757be2d1bec4d57ebc7f275a
nguyenvancong21 <nguyencong.qn21@gmail.com>
2022-08-19 09:51:05 +0700

    first commit

---
+++ QuyTrinh/QuyTrinh.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#search").autocomplete({
+        delay: 100 ,
+        source : data
+    });
+    
+    $('#my-accordion').accordion({
+        heightStyle : "content"
+    });
+
+    console.log("dsadas")
+})
+
+    
+    
